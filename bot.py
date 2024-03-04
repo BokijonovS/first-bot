5dc4d71b740ad70c4a8e38b926f7d01b65317de8
BokijonovS <sanatjon.dark@gmail.com>
2024-03-04 10:37:58 +0500

    bot qoshildi

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+from telebot import TeleBot
+from telebot.types import Message
+bot = TeleBot('6996348885:AAHfwtQz0S2E9Hg9bzRZIe4NsQn5-Bus-7I')
+@bot.message_handler(commands=['start'])
+def start(message: Message):
+    chat_id = message.chat.id
+    full_name = message.from_user.full_name
+    bot.send_message(chat_id, f'Assalomu alekum {full_name}')
+
+
+
+
+
+bot.polling()
 
